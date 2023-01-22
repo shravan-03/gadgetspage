e80d01f1e56e2c01e54b1fe4b2a5aa8080305e04
shravan <shravannkumarbavandla@gmail.com>
2023-01-22 08:37:42 +0000

    project

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+module.exports = app;
+
+app.get("/gadgets", (request, response) => {
+  response.sendFile("./gadgets.html", { root: __dirname });
+});
+app.listen(3000);
